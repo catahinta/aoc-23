427aa49e1b9f2e140342751e1d018b02f0700860
Kata Nagy <nagykata98@gmail.com>
2023-12-02 10:47:40 +0000

    Progress

--- day1/challenge.rb
+++ day1/challenge.rb
@@ -12,5 +12,15 @@ def get_results(input_array)
   sum
 end
 
-p get_results(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
-p get_results(input_array)
+get_results(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
+get_results(input_array)
+
+def get_better_results(input_array)
+  new_input = input_array.map do |input|
+    input.gsub("one", "1").gsub("two", "2").gsub("three", "3").gsub("four", "4").gsub("five", "5").gsub("six", "6").gsub("seven", "7").gsub("eight", "8").gsub("nine", "9")
+  end
+  get_results(new_input)
+end
+
+# p get_better_results(["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"])
+# p get_better_results(input_array)
